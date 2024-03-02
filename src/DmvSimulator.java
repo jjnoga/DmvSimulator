d696db2dfba647c422327a71cbf7278e37b87670
jjnoga <jjnoga@quinnipiac.edu>
2024-03-01 19:17:12 -0500

    implemented simulator

--- src/DmvSimulator.java
+++ src/DmvSimulator.java
@@ -1,5 +1,18 @@
+import java.util.Scanner;
+
 public class DmvSimulator {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Scanner scan = new Scanner(System.in);
+        int number = (int) ((Math.random() * 100) + 1);
+        System.out.println("Welcome to the DMV! You are number " + number + " in line. Please wait until your number is called.");
+        
+        int currNumber = number + 1;
+        do {
+            if (currNumber == 0) currNumber = 1;
+            System.out.println("Number " + currNumber + ", you are up next.");
+            currNumber = (currNumber + 1) % 101;
+        } while (currNumber != number);
+
+        System.out.println("Number " + currNumber + ", you are up next. \nI'm sorry, you do not have the required paperwork. \nWe hope to see you again!");
     }
 }
